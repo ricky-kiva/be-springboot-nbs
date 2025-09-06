aeac3d89b694afeb3eff4f4b8894439920f21aa6
ricky-kiva <ricky.kiva@gmail.com>
2025-09-07 00:39:48 +0700

    refactor(common-dto): set prop. as final

--- src/main/java/com/rickyslash/nbs/common/dto/ErrorResponse.java
+++ src/main/java/com/rickyslash/nbs/common/dto/ErrorResponse.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class ErrorResponse {
-  private String message;
+  private final String message;
 
   public ErrorResponse(String message) {
     this.message = message;
