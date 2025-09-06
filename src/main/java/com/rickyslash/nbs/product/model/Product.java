ca487d62dc2ad4b1888a0f39f98dacc7269d2b73
ricky-kiva <ricky.kiva@gmail.com>
2025-09-07 00:48:52 +0700

    feat(entities): add constraints to `Product` props.

--- src/main/java/com/rickyslash/nbs/product/model/Product.java
+++ src/main/java/com/rickyslash/nbs/product/model/Product.java
@@ -1,6 +1,9 @@
 package com.rickyslash.nbs.product.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
@@ -12,12 +15,15 @@ public class Product {
   @Column(name = "id")
   private Integer id;
 
+  @NotNull(message = "Name is required")
   @Column(name = "name")
   private String name;
 
+  @Size(min = 20, message = "Description must be 20 char. long")
   @Column(name = "description")
   private String description;
 
+  @PositiveOrZero(message = "Price must not be negative")
   @Column(name = "price")
   private Double price;
 }
