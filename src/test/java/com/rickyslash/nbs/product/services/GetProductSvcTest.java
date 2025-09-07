20534fcb18f843a17703e1fde22c133a037dcc26
ricky-kiva <ricky.kiva@gmail.com>
2025-09-07 10:12:16 +0700

    test(svcs.): for `GetProductSvc`

---
+++ src/test/java/com/rickyslash/nbs/product/services/GetProductSvcTest.java
@@ -0,0 +1,61 @@
+package com.rickyslash.nbs.product.services;
+
+import com.rickyslash.nbs.common.exceptions.ProductNotFoundException;
+import com.rickyslash.nbs.product.ProductRepository;
+import com.rickyslash.nbs.product.model.Product;
+import com.rickyslash.nbs.product.model.ProductDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+public class GetProductSvcTest {
+  @Mock
+  private ProductRepository productRepository;
+
+  @InjectMocks
+  private GetProductSvc getProductSvc;
+
+  @BeforeEach
+  public void setup() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  public void given_product_when_getProduct_then_return_productDTO() {
+    // Given
+    Product product = new Product();
+
+    product.setId(1);
+    product.setName("Product 1");
+    product.setDescription("Some description which is at least 20 chars");
+    product.setPrice(9.99);
+
+    when(productRepository.findById(1)).thenReturn(Optional.of(product));
+
+    // When
+    ResponseEntity<ProductDTO> res = getProductSvc.execute(product.getId());
+
+    // Then
+    assertEquals(ResponseEntity.ok(new ProductDTO(product)), res);
+    verify(productRepository, times(1)).findById(1);
+  }
+
+  @Test
+  public void given_product_doesnt_exist_when_getProduct_then_throw_exception() {
+    // Given
+    when(productRepository.findById(1)).thenReturn(Optional.empty());
+
+    // When & Then
+    assertThrows(ProductNotFoundException.class, () -> getProductSvc.execute(1));
+    verify(productRepository, times(1)).findById(1);
+  }
+}
