b7a20a9e0889622259eed36b2ba5206546b38141
ricky-kiva <ricky.kiva@gmail.com>
2025-09-08 00:52:10 +0700

    feat(app): enable caching to app

--- src/main/java/com/rickyslash/nbs/NbsApplication.java
+++ src/main/java/com/rickyslash/nbs/NbsApplication.java
@@ -2,11 +2,14 @@ package com.rickyslash.nbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class NbsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World. This is Rickyslash!");
 		SpringApplication.run(NbsApplication.class, args);
 	}
 
