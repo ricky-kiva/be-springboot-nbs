5e38113ce37153292cab593476dc6d3cb4818f4d
ricky-kiva <ricky.kiva@gmail.com>
2025-09-07 19:39:01 +0700

    feat(svcs.): create `GetCustomer` svc.

---
+++ src/main/java/com/rickyslash/nbs/customer/services/GetCustomerSvc.java
@@ -0,0 +1,30 @@
+package com.rickyslash.nbs.customer.services;
+
+import com.rickyslash.nbs.common.contract.Query;
+import com.rickyslash.nbs.common.exceptions.ErrorMessages;
+import com.rickyslash.nbs.customer.CustomerRepository;
+import com.rickyslash.nbs.customer.model.Customer;
+import com.rickyslash.nbs.customer.model.CustomerDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class GetCustomerSvc implements Query<Integer, CustomerDTO> {
+  private final CustomerRepository customerRepository;
+
+  public GetCustomerSvc(CustomerRepository customerRepository) {
+    this.customerRepository = customerRepository;
+  }
+
+  @Override
+  public ResponseEntity<CustomerDTO> execute(Integer id) {
+    Optional<Customer> optionalCustomer = customerRepository.findById(id);
+    if (optionalCustomer.isPresent()) {
+      return ResponseEntity.ok(new CustomerDTO(optionalCustomer.get()));
+    }
+
+    throw new RuntimeException(ErrorMessages.CUSTOMER_NOT_FOUND.getMessage());
+  }
+}
