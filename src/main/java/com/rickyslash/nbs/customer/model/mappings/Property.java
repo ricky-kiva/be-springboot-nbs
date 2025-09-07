38b878811cc338276a82f0ed1e323b1ce49a04e7
ricky-kiva <ricky.kiva@gmail.com>
2025-09-07 20:09:11 +0700

    feat(entities): create `Property` entity

---
+++ src/main/java/com/rickyslash/nbs/customer/model/mappings/Property.java
@@ -0,0 +1,26 @@
+package com.rickyslash.nbs.customer.model.mappings;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "property")
+@Data
+public class Property {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Integer id;
+
+  @Column(name = "street")
+  private String street;
+
+  @Column(name = "city")
+  private String city;
+
+  @Column(name = "state")
+  private String state;
+
+  @Column(name = "customer_id")
+  private Integer customerId;
+}
