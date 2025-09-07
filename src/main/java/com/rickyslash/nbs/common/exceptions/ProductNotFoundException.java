02620e757a978cc15ac4f8b7b6678561f439b878
ricky-kiva <ricky.kiva@gmail.com>
2025-09-07 21:16:55 +0700

    feat(excepts.): impl. warn log on `ProductNotFoundException`

--- src/main/java/com/rickyslash/nbs/common/exceptions/ProductNotFoundException.java
+++ src/main/java/com/rickyslash/nbs/common/exceptions/ProductNotFoundException.java
@@ -1,11 +1,14 @@
 package com.rickyslash.nbs.common.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
+@Slf4j
 public class ProductNotFoundException extends RuntimeException {
   public ProductNotFoundException() {
     super(ErrorMessages.PRODUCT_NOT_FOUND.getMessage());
+    log.error("Exception {} thrown", getClass().getSimpleName());
   }
 }
