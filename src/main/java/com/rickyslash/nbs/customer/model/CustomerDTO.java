760b6aebab3de94ef060f32b4a3f24527129da6b
ricky-kiva <ricky.kiva@gmail.com>
2025-09-07 20:58:14 +0700

    feat(dto): add `apartments` prop. to `CustomerDTO`

--- src/main/java/com/rickyslash/nbs/customer/model/CustomerDTO.java
+++ src/main/java/com/rickyslash/nbs/customer/model/CustomerDTO.java
@@ -1,5 +1,6 @@
 package com.rickyslash.nbs.customer.model;
 
+import com.rickyslash.nbs.customer.model.mappings.ApartmentDTO;
 import com.rickyslash.nbs.customer.model.mappings.HomeDTO;
 import com.rickyslash.nbs.customer.model.mappings.PropertyDTO;
 import lombok.Data;
@@ -12,6 +13,7 @@ public class CustomerDTO {
   private String lastName;
   private HomeDTO home;
   private List<PropertyDTO> properties;
+  private List<ApartmentDTO> apartments;
 
   public CustomerDTO(Customer customer) {
     this.firstName = customer.getFirstname();
@@ -22,6 +24,10 @@ public class CustomerDTO {
 
     this.properties = (customer.getProperties() != null)
         ? customer.getProperties().stream().map(PropertyDTO::new).toList()
+        : null;
+
+    this.apartments = (customer.getApartments() != null)
+        ? customer.getApartments().stream().map(ApartmentDTO::new).toList()
         : null;
   }
 }
