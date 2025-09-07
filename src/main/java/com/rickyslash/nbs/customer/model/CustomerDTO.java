4fd71ddab6539e16bb0a3b6bfd5ce93fba19eecb
ricky-kiva <ricky.kiva@gmail.com>
2025-09-07 20:19:37 +0700

    feat(model): establish one `Customer` to many `Properties`

--- src/main/java/com/rickyslash/nbs/customer/model/CustomerDTO.java
+++ src/main/java/com/rickyslash/nbs/customer/model/CustomerDTO.java
@@ -1,19 +1,27 @@
 package com.rickyslash.nbs.customer.model;
 
 import com.rickyslash.nbs.customer.model.mappings.HomeDTO;
+import com.rickyslash.nbs.customer.model.mappings.PropertyDTO;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class CustomerDTO {
   private String firstName;
   private String lastName;
   private HomeDTO home;
+  private List<PropertyDTO> properties;
 
   public CustomerDTO(Customer customer) {
     this.firstName = customer.getFirstname();
     this.lastName = customer.getLastname();
     this.home = (customer.getHome() != null)
-      ? new HomeDTO(customer.getHome())
-      : null;
+        ? new HomeDTO(customer.getHome())
+        : null;
+
+    this.properties = (customer.getProperties() != null)
+        ? customer.getProperties().stream().map(PropertyDTO::new).toList()
+        : null;
   }
 }
