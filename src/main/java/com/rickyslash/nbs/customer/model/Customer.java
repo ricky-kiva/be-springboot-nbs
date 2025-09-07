e97228696aac6608985d2a7010c7660a36f3e40f
ricky-kiva <ricky.kiva@gmail.com>
2025-09-07 20:10:32 +0700

    feat(model): establish one `Customer` to many `Properties`

--- src/main/java/com/rickyslash/nbs/customer/model/Customer.java
+++ src/main/java/com/rickyslash/nbs/customer/model/Customer.java
@@ -1,8 +1,11 @@
 package com.rickyslash.nbs.customer.model;
 
 import com.rickyslash.nbs.customer.model.mappings.Home;
+import com.rickyslash.nbs.customer.model.mappings.Property;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.List;
 
 @Entity
 @Table(name = "customer")
@@ -22,4 +25,8 @@ public class Customer {
   @OneToOne(cascade = CascadeType.ALL)
   @JoinColumn(name = "home_id")
   private Home home;
+
+  @OneToMany(cascade = CascadeType.ALL)
+  @JoinColumn(name = "customer_id")
+  private List<Property> properties;
 }
