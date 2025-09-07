f357316806c091ef3f2d9110e2bf31bb3a26939b
ricky-kiva <ricky.kiva@gmail.com>
2025-09-07 10:44:02 +0700

    refactor(svcs.): use `switch` instd.

--- src/main/java/com/rickyslash/nbs/header/services/GetRegionHeaderResponseSvc.java
+++ src/main/java/com/rickyslash/nbs/header/services/GetRegionHeaderResponseSvc.java
@@ -8,17 +8,12 @@ import org.springframework.stereotype.Service;
 public class GetRegionHeaderResponseSvc implements Query<String, String> {
   @Override
   public ResponseEntity<String> execute(String region) {
-    String res = null;
-
-    if (region.equals("ID")) {
-      res = "Rayuan Pulau Kelapa";
-    } else if (region.equals("US")) {
-      res = "BALD EAGLE FREEDOM";
-    } else if (region.equals("CAN")) {
-      res = "MAPLE SYRUP";
-    } else {
-      res = "Country not supported";
-    }
+    String res = switch (region) {
+      case "ID" -> "Rayuan Pulau Kelapa";
+      case "US" -> "BALD EAGLE FREEDOM";
+      case "CAN" -> "MAPLE SYRUP";
+      default -> "Country not supported";
+    };
 
     return ResponseEntity.ok(res);
   }
