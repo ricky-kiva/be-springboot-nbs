12d4e5f5f2ffa8e7245a5632bfab606392ac6169
ricky-kiva <ricky.kiva@gmail.com>
2025-09-09 02:52:39 +0700

    feat(secur.): impl. `JwtAuthenticationFilter`

--- src/main/java/com/rickyslash/nbs/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/com/rickyslash/nbs/infrastructure/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.rickyslash.nbs.infrastructure.security;
 
+import com.rickyslash.nbs.infrastructure.security.jwt.filters.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,7 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
@@ -31,13 +31,19 @@ public class SecurityConfiguration {
     return httpSecurity
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(authorize -> {
+          authorize.requestMatchers("/login").permitAll();
           authorize.requestMatchers("/register").permitAll();
           authorize.anyRequest().authenticated();
         })
         .addFilterBefore(
-            new BasicAuthenticationFilter(authenticationManager(httpSecurity)),
+            jwtAuthenticationFilter(),
             UsernamePasswordAuthenticationFilter.class
         )
         .build();
+  }
+
+  @Bean
+  public JwtAuthenticationFilter jwtAuthenticationFilter() {
+    return new JwtAuthenticationFilter();
   }
 }
