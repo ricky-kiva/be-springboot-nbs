dcbb907b476fc7bc8afb11b8d85d40e98f168d35
ricky-kiva <ricky.kiva@gmail.com>
2025-09-08 21:17:38 +0700

    feat(security): create `superuser` user & auth. any req.

--- src/main/java/com/rickyslash/nbs/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/com/rickyslash/nbs/infrastructure/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
@@ -15,9 +16,12 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfiguration {
   String AUTHORITY_ADMIN = "ADMIN";
   String AUTHORITY_USER = "USER";
+  String ROLE_SUPERUSER = "superuser";
+  String ROLE_BASIC_USER = "basicuser";
 
   @Bean
   public UserDetailsService userDetailsService(PasswordEncoder encoder) {
@@ -25,21 +29,28 @@ public class SecurityConfiguration {
     String ADMIN_PASSWORD = "1";
     String USER_USERNAME = "user";
     String USER_PASSWORD = "2";
-
+    String SUPERUSER_USERNAME = "rickyslash";
+    String SUPERUSER_PASSWORD = "99";
 
     UserDetails admin = User
         .withUsername(ADMIN_USERNAME)
-        .authorities(AUTHORITY_ADMIN, AUTHORITY_USER)
+        .authorities(AUTHORITY_ADMIN, AUTHORITY_USER, roleToAuthority(ROLE_BASIC_USER))
         .password(encoder.encode(ADMIN_PASSWORD))
         .build();
 
     UserDetails user = User
         .withUsername(USER_USERNAME)
-        .authorities(AUTHORITY_USER)
+        .authorities(AUTHORITY_USER, roleToAuthority(ROLE_BASIC_USER))
         .password(encoder.encode(USER_PASSWORD))
         .build();
 
-    return new InMemoryUserDetailsManager(admin, user);
+    UserDetails superuser = User
+        .withUsername(SUPERUSER_USERNAME)
+        .authorities(AUTHORITY_ADMIN, AUTHORITY_USER, roleToAuthority(ROLE_SUPERUSER))
+        .password(encoder.encode(SUPERUSER_PASSWORD))
+        .build();
+
+    return new InMemoryUserDetailsManager(admin, user, superuser);
   }
 
   @Bean
@@ -53,13 +64,18 @@ public class SecurityConfiguration {
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(authorize -> {
           authorize.requestMatchers("/open").permitAll();
-          authorize.requestMatchers("/closed").authenticated();
           authorize.requestMatchers(HttpMethod.POST, "/product").authenticated();
           authorize.requestMatchers(HttpMethod.GET, "/admin-dashboard").hasAuthority(AUTHORITY_ADMIN);
           authorize.requestMatchers(HttpMethod.GET, "/user-dashboard")
               .hasAnyAuthority(AUTHORITY_USER, AUTHORITY_ADMIN);
+
+          authorize.anyRequest().authenticated();
         })
         .httpBasic(Customizer.withDefaults())
         .build();
+  }
+
+  private String roleToAuthority(String role) {
+    return "ROLE_" + role;
   }
 }
