095608e5ab871fc2abd13312a4d331e1aae139f7
ricky-kiva <ricky.kiva@gmail.com>
2025-09-08 20:02:19 +0700

    feat(security): create `user` `UserDetails` & authorities for `admin` & `user`

--- src/main/java/com/rickyslash/nbs/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/com/rickyslash/nbs/infrastructure/security/SecurityConfiguration.java
@@ -16,17 +16,30 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
+  String AUTHORITY_ADMIN = "ADMIN";
+  String AUTHORITY_USER = "USER";
+
   @Bean
   public UserDetailsService userDetailsService(PasswordEncoder encoder) {
     String ADMIN_USERNAME = "admin";
     String ADMIN_PASSWORD = "1";
+    String USER_USERNAME = "user";
+    String USER_PASSWORD = "2";
+
 
     UserDetails admin = User
         .withUsername(ADMIN_USERNAME)
+        .authorities(AUTHORITY_ADMIN, AUTHORITY_USER)
         .password(encoder.encode(ADMIN_PASSWORD))
         .build();
 
-    return new InMemoryUserDetailsManager(admin);
+    UserDetails user = User
+        .withUsername(USER_USERNAME)
+        .authorities(AUTHORITY_USER)
+        .password(encoder.encode(USER_PASSWORD))
+        .build();
+
+    return new InMemoryUserDetailsManager(admin, user);
   }
 
   @Bean
@@ -42,6 +55,9 @@ public class SecurityConfiguration {
           authorize.requestMatchers("/open").permitAll();
           authorize.requestMatchers("/closed").authenticated();
           authorize.requestMatchers(HttpMethod.POST, "/product").authenticated();
+          authorize.requestMatchers(HttpMethod.GET, "/admin-dashboard").hasAuthority(AUTHORITY_ADMIN);
+          authorize.requestMatchers(HttpMethod.GET, "/user-dashboard")
+              .hasAnyAuthority(AUTHORITY_USER, AUTHORITY_ADMIN);
         })
         .httpBasic(Customizer.withDefaults())
         .build();
