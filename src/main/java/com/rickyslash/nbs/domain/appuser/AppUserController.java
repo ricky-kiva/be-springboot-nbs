4eced91d1678516fd9e97ed9b735aa476a0965fe
ricky-kiva <ricky.kiva@gmail.com>
2025-09-09 02:51:42 +0700

    feat(ctrls.): add `/login` endpoint

--- src/main/java/com/rickyslash/nbs/domain/appuser/AppUserController.java
+++ src/main/java/com/rickyslash/nbs/domain/appuser/AppUserController.java
@@ -2,6 +2,7 @@ package com.rickyslash.nbs.domain.appuser;
 
 import com.rickyslash.nbs.domain.appuser.model.AppUser;
 import com.rickyslash.nbs.domain.appuser.services.CreateNewAppUserSvc;
+import com.rickyslash.nbs.domain.appuser.services.LoginAppUserSvc;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,9 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AppUserController {
   private final CreateNewAppUserSvc createNewAppUserSvc;
+  private final LoginAppUserSvc loginAppUserSvc;
 
-  public AppUserController(CreateNewAppUserSvc createNewAppUserSvc) {
+  public AppUserController(CreateNewAppUserSvc createNewAppUserSvc, LoginAppUserSvc loginAppUserSvc) {
     this.createNewAppUserSvc = createNewAppUserSvc;
+    this.loginAppUserSvc = loginAppUserSvc;
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<String> loginUser(@RequestBody AppUser user) {
+    return loginAppUserSvc.execute(user);
   }
 
   @PostMapping("/register")
