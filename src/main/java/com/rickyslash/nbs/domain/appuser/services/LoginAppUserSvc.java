ac6f369efba2e97c24f1cdbc397752f4b761523d
ricky-kiva <ricky.kiva@gmail.com>
2025-09-09 02:51:01 +0700

    feat(svcs.): create `LoginAppUser` svc.

---
+++ src/main/java/com/rickyslash/nbs/domain/appuser/services/LoginAppUserSvc.java
@@ -0,0 +1,39 @@
+package com.rickyslash.nbs.domain.appuser.services;
+
+import com.rickyslash.nbs.common.contract.Query;
+import com.rickyslash.nbs.domain.appuser.model.AppUser;
+import com.rickyslash.nbs.infrastructure.security.jwt.JwtUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginAppUserSvc implements Query<AppUser, String> {
+  private final AuthenticationManager authenticationManager;
+
+  public LoginAppUserSvc(AuthenticationManager authenticationManager) {
+    this.authenticationManager = authenticationManager;
+  }
+
+  @Override
+  public ResponseEntity<String> execute(AppUser user) {
+    UsernamePasswordAuthenticationToken springSecToken = new UsernamePasswordAuthenticationToken(
+        user.getUsername(),
+        user.getPassword()
+    );
+
+    Authentication authentication = authenticationManager.authenticate(springSecToken);
+
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+
+    User userPrincipal = (User) authentication.getPrincipal();
+
+    String jwtToken = JwtUtil.generateToken(userPrincipal);
+
+    return ResponseEntity.ok(jwtToken);
+  }
+}
