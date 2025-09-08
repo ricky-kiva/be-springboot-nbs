6ac2a13525b245fb01ea847b591b260487c8cb42
ricky-kiva <ricky.kiva@gmail.com>
2025-09-08 17:09:49 +0700

    feat(infra): create Security Filter Chain for authorz. on Basic auth.

--- src/main/java/com/rickyslash/nbs/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/com/rickyslash/nbs/infrastructure/security/SecurityConfiguration.java
@@ -2,12 +2,15 @@ package com.rickyslash.nbs.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
@@ -27,5 +30,16 @@ public class SecurityConfiguration {
   @Bean
   PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity
+        .authorizeHttpRequests(authorize -> {
+          authorize.requestMatchers("/open").permitAll();
+          authorize.requestMatchers("/closed").authenticated();
+        })
+        .httpBasic(Customizer.withDefaults())
+        .build();
   }
 }
