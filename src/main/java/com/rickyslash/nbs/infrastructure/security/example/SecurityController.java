2c7d5a8a041587f1eb0801b3912db96fdcf9378f
ricky-kiva <ricky.kiva@gmail.com>
2025-09-08 21:19:13 +0700

    feat(security): create example endpoint with `@PreAuthorize` by validating Role

--- src/main/java/com/rickyslash/nbs/infrastructure/security/example/SecurityController.java
+++ src/main/java/com/rickyslash/nbs/infrastructure/security/example/SecurityController.java
@@ -1,5 +1,6 @@
 package com.rickyslash.nbs.infrastructure.security.example;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,17 @@ public class SecurityController {
   @GetMapping("/user-dashboard")
   public String userDashboard() {
     return "HELLO USER";
+  }
+
+  @PreAuthorize("hasRole('superuser')")
+  @GetMapping("/terminal")
+  public String terminal() {
+    return "IN TERMINAL";
+  }
+
+  @PreAuthorize("hasRole('basicuser') or hasRole('superuser')")
+  @GetMapping("/cloud-services")
+  public String cloudServices() {
+    return "IN CLOUD SERVICES DASHBOARD";
   }
 }
